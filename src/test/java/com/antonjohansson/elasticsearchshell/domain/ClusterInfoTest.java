a00445020a203b0ba383c885db717f0a6220d122
Anton Johansson <antoon.johansson@gmail.com>
2017-04-30 18:29:45 +0200

    Add base test for testing domain objects Add test for cluster info

---
+++ src/test/java/com/antonjohansson/elasticsearchshell/domain/ClusterInfoTest.java
@@ -0,0 +1,8 @@
+package com.antonjohansson.elasticsearchshell.domain;
+
+/**
+ * Unit tests of {@link ClusterInfo}.
+ */
+public class ClusterInfoTest extends AbstractDomainTest<ClusterInfo>
+{
+}
---
+++ src/test/java/com/antonjohansson/elasticsearchshell/domain/ClusterInfoVersionTest.java
@@ -0,0 +1,8 @@
+package com.antonjohansson.elasticsearchshell.domain;
+
+/**
+ * Unit tests of {@link ClusterInfo.Version}.
+ */
+public class ClusterInfoVersionTest extends AbstractDomainTest<ClusterInfo.Version>
+{
+}
