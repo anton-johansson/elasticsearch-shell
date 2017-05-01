ad98d594a077e1df36b5e838f77441c034001445
Anton Johansson <antoon.johansson@gmail.com>
2017-05-01 21:58:16 +0200

    Add unit tests to cover exit and help commands

---
+++ src/test/java/com/antonjohansson/elasticsearchshell/shell/commands/core/ExitCommandTest.java
@@ -0,0 +1,37 @@
+/**
+ *    Copyright 2017 Anton Johansson
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.antonjohansson.elasticsearchshell.shell.commands.core;
+
+import org.junit.Test;
+import org.springframework.shell.core.CommandResult;
+import org.springframework.shell.core.ExitShellRequest;
+
+/**
+ * Unit tests of {@link ExitCommand}.
+ */
+public class ExitCommandTest extends AbstractCommandTest<ExitCommand>
+{
+    @Test
+    public void test()
+    {
+        CommandResult result = shell().executeCommand("exit");
+        assertTrue(result.isSuccess());
+
+        ExitShellRequest expected = ExitShellRequest.NORMAL_EXIT;
+        ExitShellRequest actual = (ExitShellRequest) result.getResult();
+        assertEquals(expected, actual);
+    }
+}
---
+++ src/test/java/com/antonjohansson/elasticsearchshell/shell/commands/core/HelpCommandTest.java
@@ -0,0 +1,32 @@
+/**
+ *    Copyright 2017 Anton Johansson
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.antonjohansson.elasticsearchshell.shell.commands.core;
+
+import org.junit.Test;
+import org.springframework.shell.core.CommandResult;
+
+/**
+ * Unit tests of {@link HelpCommand}.
+ */
+public class HelpCommandTest extends AbstractCommandTest<HelpCommand>
+{
+    @Test
+    public void test()
+    {
+        CommandResult result = shell().executeCommand("help");
+        assertTrue(result.isSuccess());
+    }
+}
